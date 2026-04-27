1cafbafd20881f08e94ee37f9748e50f03247d64
unknown <mdasifalijihat@gmail.com>
2026-04-27 17:41:55 +0600

    added fiexd win history change bet history

--- src/app/(user)/kalyan/[playType]/[marketId]/page.tsx
+++ src/app/(user)/kalyan/[playType]/[marketId]/page.tsx
@@ -358,7 +358,7 @@ export default function GamePlayPage() {
       if (res.success) {
         toast.success("Bet placed successfully!");
         reset();
-        router.push("/kalyan/win-history");
+        router.push("/kalyan/bet-history");
       } else {
         toast.error(res.message ?? "Submission failed. Try again.");
       }
